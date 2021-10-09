a13b0846a4d6c49491f2b71cb0d76c5730c57a1a
zexuan <syltaris@gmail.com>
2021-10-09 14:28:10 +0800

    update apis to new format

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,9 +1,9 @@
 export const getMapByHash = async (hash) => {
-  return (await fetch(`https://beatsaver.com/api/maps/by-hash/${hash}`)).json();
+  return (await fetch(`https://beatsaver.com/api/maps/hash/${hash}`)).json();
 };
 
-export const getMapByKey = async (songKey) => {
-  return await fetch(`https://beatsaver.com/api/maps/detail/${songKey}`);
+export const getMapById = async (id) => {
+  return await fetch(`https://beatsaver.com/api/maps/id/${id}`);
 };
 
 export const getBeatSaverMapList = async (page, type) => {
